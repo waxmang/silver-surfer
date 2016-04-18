e90c9c1563c1be78c80227595d3047e2c5a6b703
Max Wang <maxwang051@gmail.com>
2016-04-17 23:30:01 -0500

    removed stripe AGAIN, added paypal donation

--- sendmessage/views.py
+++ sendmessage/views.py
@@ -15,45 +15,27 @@ def index(request):
     return render(request, 'sendmessage/index.html', {})
 
 def send(request):
-    phone_number1 = request.POST['number1']
-    phone_number2 = request.POST['number2']
-    phone_number3 = request.POST['number3']
-
-    if (len(phone_number1)<10):
+    phone_number = request.POST['number']
+    if (len(phone_number)<10):
         return render(request, 'sendmessage/index.html', {
             'error_message': 'You didn\'t enter a phone number.',
         })
     else:
-        token = request.POST['stripeToken']
-         #Create the charge on Stripe's servers - this will charge the user's card
-        try:
-          charge = stripe.Charge.create(
-             amount=100, # amount in cents, again
-             currency="usd",
-             source=token,
-             description="Just keeping it loopy"
-        )
-        except stripe.error.CardError, e:
-            pass
+        #token = request.POST['stripeToken']
+        # Create the charge on Stripe's servers - this will charge the user's card
+        #try:
+        #  charge = stripe.Charge.create(
+        #     amount=100, # amount in cents, again
+        #      currency="usd",
+        #      source=token,
+        #      description="Just keeping it loopy"
+        # )
+        #except stripe.error.CardError, e:
+          # The card has been declined
+        #  pass
 
         try:
-            call = client.calls.create(to=phone_number1,
-                from_="+15103533372",
-                url="http://twimlbin.com/external/5efadf9718cccc0aebb0ea18dd2ad717")
-            print call.sid
-        except Exception, e:
-            print str(e)
-
-        try:
-            call = client.calls.create(to=phone_number2,
-                from_="+15103533372",
-                url="http://twimlbin.com/external/5efadf9718cccc0aebb0ea18dd2ad717")
-            print call.sid
-        except Exception, e:
-            print str(e)
-
-        try:
-            call = client.calls.create(to=phone_number3,
+            call = client.calls.create(to=phone_number,
                 from_="+15103533372",
                 url="http://twimlbin.com/external/5efadf9718cccc0aebb0ea18dd2ad717")
             print call.sid
